189fea0b2b2b521422d1fc173519f96ad5fb141e
Alko <xcv3549@gmail.com>
2021-04-15 19:16:56 +0900

    First Commit: Change Repository

---
+++ accountapp/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
+# Create your views here.
+
+
+
+def hello_world(request):
+    if request.method == "POST":
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list' : hello_world_list})
+
