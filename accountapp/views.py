39718d596246a429ff8b992702b6562b3293f6fc
Alko <xcv3549@gmail.com>
2021-04-20 09:24:38 +0900

    Django course 28: Add Authenticated with Decorator

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,16 +1,21 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
+from accountapp.decorator import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 # Create your views here.
 
+has_ownership = [account_ownership_required, login_required]
 
-
+@login_required
 def hello_world(request):
     if request.method == "POST":
 
@@ -40,6 +45,8 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class  AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -47,6 +54,8 @@ class  AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+@method_decorator(has_ownership,'get')
+@method_decorator(has_ownership,'post')
 class  AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
