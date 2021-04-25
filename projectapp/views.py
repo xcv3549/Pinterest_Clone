ce43ec2b20d2e64ec5de259e7a6db214dbbd13b7
Alko <xcv3549@gmail.com>
2021-04-25 11:07:11 +0900

    Django course 42: Subscribe App with Redirect View

--- projectapp/views.py
+++ projectapp/views.py
@@ -10,6 +10,7 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.models import Project
 from projectapp.forms import ProjectCreationForm
+from subscribeapp.models import Subscription
 
 
 class ProjectListView(ListView):
@@ -36,5 +37,11 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 25
 
     def get_context_data(self, **kwargs):
+        project = self.object
+        user = self.request.user
+
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+
         object_list = Article.objects.filter(project=self.get_object())
-        return super(ProjectDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list, subscription=subscription, **kwargs)
